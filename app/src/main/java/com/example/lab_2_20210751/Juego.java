b7f8e81c3cfdbf2d4d3ee104f0837b0465a275fa
Santy2304 <a20210751@pucp.edu.pe>
2024-09-07 02:06:46 -0500

    ayuda

--- app/src/main/java/com/example/lab_2_20210751/Juego.java
+++ app/src/main/java/com/example/lab_2_20210751/Juego.java
@@ -1,6 +1,13 @@
 package com.example.lab_2_20210751;
 
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,17 +15,69 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.Random;
+
 public class Juego extends AppCompatActivity {
+
+    private String[] palabras;
+    private Random random;
+    private TextView[] letra;
+    private LinearLayout linearLayout;
+
+    private ImageView cabeza;
+    private ImageView torso;
+    private ImageView brazoDer;
+    private ImageView brazoIz;
+    private ImageView piernaDer;
+    private ImageView piernaIz;
+
+    private int vidas = 6;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_juego);
+        palabras = getResources().getStringArray(R.array.palabras);
+        linearLayout=findViewById(R.id.linearLayout);
+        random = new Random();
+        elegirPalabra();
+
+        cabeza = findViewById(R.id.cabeza);
+        torso = findViewById(R.id.torso);
+        brazoDer = findViewById(R.id.brazoDere);
+        brazoIz = findViewById(R.id.brazoIz);
+        piernaIz = findViewById(R.id.piernaIz);
+        piernaDer = findViewById(R.id.piernaDer);
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
+
+    private void elegirPalabra(){
+        String palabraElegida = palabras[random.nextInt(palabras.length)];
+        Log.d("Juego", "Palabra seleccionada: " + palabraElegida);
+
+        letra = new TextView[palabraElegida.length()];
+
+        for (int i=0; i<palabraElegida.length();i++){
+            letra[i] = new TextView(this);
+            letra[i].setText("");
+            letra[i].setGravity(Gravity.CENTER);
+            letra[i].setBackgroundResource(R.drawable.pngwing_com);
+            //Código usado de ChatGPT para añadir margenes entre las letras
+            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(80, 80);
+            params.setMargins(20, 5, 20, 5);
+            letra[i].setLayoutParams(params);
+
+            linearLayout.addView(letra[i]);
+        }
+
+    }
+
+
 }
--- app/src/main/java/com/example/lab_2_20210751/MainActivity.java
+++ app/src/main/java/com/example/lab_2_20210751/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.lab_2_20210751;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.widget.Button;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,6 +20,36 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
+
+        Button jugar = findViewById(R.id.button);
+        jugar.setEnabled(false);
+
+        EditText nombre = findViewById(R.id.editTextText2);
+
+        nombre.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                jugar.setEnabled(charSequence.length() > 0);
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+
+            }
+        });
+
+        jugar.setOnClickListener(view -> {
+            Intent intent = new Intent(this, Juego.class);
+            startActivity(intent);
+        });
+
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
